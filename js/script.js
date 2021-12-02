813508c22b208bced2ea31f3262065df69455dfe
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2021-12-02 15:06:35 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 // a chiedo il nome
+const userName = prompt('Inserisci il tuo nome');
 
 // b chiedo il cognome
-
+const userLastname = prompt('Inserisci il tuo cognome');
 // c chiedo il colore preferito
-
-// d chiedo l'anno di nascita
+const mainColor = prompt('Inserisci il tuo coolore preferito');
+// aggiungo l'anno corrente
 
 // e genero la password con a + b + c + d
+let htmlElement = document.getElementById('pw-generate');
+htmlElement.innerHTML = `${userName}${userLastname}${mainColor}21`;
